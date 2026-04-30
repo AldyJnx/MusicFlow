ce24e0dfffd51d144c09243ea6ddb87d4b81b9e8
Strike32GT <roberto.mas@tecsup.edu.pe>
2026-04-30 08:53:10 -0500

    Navbar reutilizable (web)

--- apps/web/src/client/layout/ClientLayout.tsx
+++ apps/web/src/client/layout/ClientLayout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react'
 import { useState } from 'react'
+import Navbar from '../../shared/ui/navigation/Navbar'
 import Sidebar from '../../shared/ui/navigation/Sidebar'
 import { sidebarClient } from '../components/navigation/sidebarClient'
 
@@ -13,7 +14,10 @@ export default function ClientLayout({ children }: { children: ReactNode }) {
         collapsed={collapsed}
         onToggleCollapse={() => setCollapsed((current) => !current)}
       />
-      <main className="flex-1">{children}</main>
+      <div className="flex min-h-screen flex-1 flex-col">
+        <Navbar />
+        <main className="flex-1">{children}</main>
+      </div>
     </div>
   )
 }
