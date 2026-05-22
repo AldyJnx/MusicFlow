1a1d2a985da7e445f3dddf6a8c3b97ad7e967b32
Strike32GT <roberto.mas@tecsup.edu.pe>
2026-05-21 23:58:25 -0500

    Actualizando el routes

--- apps/mobile/lib/app/routes.dart
+++ apps/mobile/lib/app/routes.dart
@@ -1,6 +1,7 @@
 abstract final class AppRoutes {
   static const String home = '/';
   static const String playlists = '/playlists';
+  static const String equalizer = '/equalizer';
   static const String profile = '/profile';
   static const String premium = '/premium';
 }
